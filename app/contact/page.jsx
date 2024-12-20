7d406d49c3563a7a16ef388909d4afeea7a3bcba
Rama Zuliyansyah <40348070+ramazuliyansyah@users.noreply.github.com>
2024-12-20 15:39:18 +0700

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -56,9 +56,9 @@ const Contact = () => {
           {/* Form */}
           <div className="xl:h-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-accent">Let's work together</h3>
+              <h3 className="text-4xl text-accent">Let&#39;s work together</h3>
               <p className="text-white/60">
-                Innovation starts here. Let's build something incredible
+                Innovation starts here. Let&#39;s build something incredible
                 together.
               </p>
               {/* Input */}
