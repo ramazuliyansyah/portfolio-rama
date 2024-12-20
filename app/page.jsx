34977f8b4c43d7ddf68b6bdbcdae8b0e37ea308c
Rama Zuliyansyah <40348070+ramazuliyansyah@users.noreply.github.com>
2024-12-20 15:38:45 +0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -13,7 +13,7 @@ const Home = () => {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">IT Support & Jr. Web Developer</span>
             <h1 className="h1 mb-6">
-              Hello I'm <br />{" "}
+              Hello I&#39;m <br />{" "}
               <span className="text-accent">Rama Zuliyansyah</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
